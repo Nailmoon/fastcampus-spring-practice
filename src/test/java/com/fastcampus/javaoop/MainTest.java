73dde0326ed05414f057ee4a788e2af4a0337400
neilmoon <kamulai@naver.com>
2021-08-25 22:41:01 +0900

    Initialize #1 - java project
    지난 시간 만들었던 것을 복사해옴
    * https://github.com/Nailmoon/fastcampus-java-oop

---
+++ src/test/java/com/fastcampus/javaoop/MainTest.java
@@ -0,0 +1,19 @@
+package com.fastcampus.javaoop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        //Given
+        String[] args = {"3", "2", "1"};
+
+        //When
+        Main.main(args);
+
+        //Then
+    }
+}
