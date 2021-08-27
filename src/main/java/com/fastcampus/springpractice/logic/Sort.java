dc3c3f2d1cab18cd6859963e8b82d33405f9fc5d
neilmoon <kamulai@naver.com>
2021-08-27 09:45:06 +0900

    Change #1 - package naming
    복사를 그대로 했기 때문에 패키지명 변경

--- src/main/java/com/fastcampus/springpractice/logic/Sort.java
+++ src/main/java/com/fastcampus/springpractice/logic/Sort.java
@@ -1,7 +1,10 @@
-package com.fastcampus.javaoop.logic;
+package com.fastcampus.springpractice.logic;
+
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface Sort <T extends  Comparable<T>>{
     List<T> sort(List<T> list);
 }
--- src/test/java/com/fastcampus/springpractice/MainTest.java
+++ src/test/java/com/fastcampus/springpractice/MainTest.java
@@ -1,15 +1,13 @@
-package com.fastcampus.javaoop;
+package com.fastcampus.springpractice;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 
     @Test
     void main() {
         //Given
-        String[] args = {"3", "2", "1"};
+        String[] args = {"3", "1", "2"};
 
         //When
         Main.main(args);
